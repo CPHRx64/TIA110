73ad638ecb0a2b9f78c4d1d823b04c934490f0ea
imjustaboi <dophamcao@hotmail.com>
2020-10-16 11:27:05 +0200

    blue functionalities added

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.myapplication
 
+import android.app.Activity
 import android.bluetooth.BluetoothAdapter
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val REQUEST_CODE_ENABLE_BT:Int = 1;
+    private val REQUEST_CODE_DISCOVERABLE_BT:Int = 2;
 
     //bluetooth adapter
     lateinit var bAdapter:BluetoothAdapter
@@ -32,8 +37,72 @@ class MainActivity : AppCompatActivity() {
             bluetoothStatusIv.setImageResource(R.drawable.ic_bluetooth_off)
         }
 
+        // turn on/off bluetooth
+        turnOnBtn.setOnClickListener {
+            if (bAdapter.isEnabled){
+                //already enabled
+                Toast.makeText(this, "Already on", Toast.LENGTH_LONG).show()
+            }else{
+                // turn on bluetooth
+                val intent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                startActivityForResult(intent, REQUEST_CODE_ENABLE_BT)
+            }
+        }
+
+        turnOffBtn.setOnClickListener {
+            if (!bAdapter.isEnabled){
+                //already enabled
+                Toast.makeText(this, "Already off", Toast.LENGTH_LONG).show()
+            }else{
+                // turn on bluetooth
+                bAdapter.disable()
+                bluetoothStatusIv.setImageResource(R.drawable.ic_bluetooth_off)
+                Toast.makeText(this, "Bluetooth turned off", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        // discoverable
+        discoverableBtn.setOnClickListener {
+            if(bAdapter.isDiscovering) {
+                Toast.makeText(this, "Making your device discoverable", Toast.LENGTH_LONG).show()
+                val intent = Intent(Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE))
+                startActivityForResult(intent, REQUEST_CODE_DISCOVERABLE_BT)
+            }
+        }
+
+        // get list of paired devices
+        pairedBtn.setOnClickListener{
+            if (bAdapter.isEnabled) {
+                pairedTv.text="Paired devices"
+                // get list of paired devices
+                val devices = bAdapter.bondedDevices
+                for (device in devices){
+                    val deviceName = device.name
+                    pairedTv.append("\nDevice: $deviceName, $device")
+                }
+            }else{
+                Toast.makeText(this, "turn on bluetooth first", Toast.LENGTH_LONG).show()
+            }
+        }
+
+
+
         //val connectButton: Button = findViewById(R.id.connectButton)
         //connectButton.setOnClickListener { connect() }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (requestCode) {
+            REQUEST_CODE_ENABLE_BT ->
+                if(resultCode == Activity.RESULT_OK) {
+                    bluetoothStatusIv.setImageResource(R.drawable.ic_bluetooth_on)
+                    Toast.makeText(this, "Bluetooth is on", Toast.LENGTH_LONG).show()
+                }else{
+                    // user denied to turn on bluetooth from popup
+                    Toast.makeText(this, "Couldn't turn on bluetooth", Toast.LENGTH_LONG).show()
+                }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
     private fun connect() {
