18a8d6195b84a6e1c9878adedc40c58d433ee1f5
imjustaboi <dophamcao@hotmail.com>
2020-10-19 13:43:39 +0200

    distance between 2 locations

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,15 +3,32 @@ package com.example.myapplication
 import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
+import androidx.annotation.Nullable
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.datatransport.runtime.backends.BackendResponse
+import com.google.android.gms.common.api.Status
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.Autocomplete
+import com.google.android.libraries.places.widget.AutocompleteActivity
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import javax.net.ssl.SSLEngineResult
+
 
 class MainActivity : AppCompatActivity() {
     private val REQUEST_CODE_ENABLE_BT:Int = 1;
     private val REQUEST_CODE_DISCOVERABLE_BT:Int = 2;
+    private var sType : String = ""
+    private var lat0 : Double = 0.0
+    private var long0 : Double = 0.0
+    private var lat1 : Double = 0.0
+    private var long1 : Double = 0.0
+    private var flag : Int = 0;
+
 
     //bluetooth adapter
     lateinit var bAdapter:BluetoothAdapter
@@ -19,13 +36,53 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //init google places:
+        Places.initialize(applicationContext, "AIzaSyD7su_-5oHGyppir4oCcwG3jUFPMjVshMw")
+
+        // non focusable edit text
+        geo_source.isFocusable = false
+        geo_source.setOnClickListener{
+            // define type
+            sType = "source"
+            // init place field list
+            val placeFields: List<Place.Field> = Arrays.asList(Place.Field.ADDRESS, Place.Field.LAT_LNG)
+
+            // create intent
+            val intent : Intent = Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY, placeFields).build(this)
+
+            // start activity result
+            startActivityForResult(intent, 100)
+        }
+
+        // set destination
+        geo_destination.isFocusable = false
+        geo_destination.setOnClickListener {
+            // define type
+            sType = "destination"
+
+            // init places
+            val placeFields: List<Place.Field> = Arrays.asList(Place.Field.ADDRESS, Place.Field.LAT_LNG)
+
+            // create intent
+            val intent : Intent = Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY, placeFields).build(this)
+
+            // start activity result
+            startActivityForResult(intent, 100)
+
+        }
+
+        // set distance view
+        geo_distance.setText("0.0 Km")
+
+
+        //-------------------------------------------------------------------------------//
+
         // init bluetooth adapter
         bAdapter = BluetoothAdapter.getDefaultAdapter()
         //check bluetooth status
         if (bAdapter == null) {
             bluetoothStatusTv.text = "Bluetooth is not available"
         } else {
-            bluetoothStatusTv.text = "Bluetooth is available"
         }
 
         // set icon accordingly
@@ -85,10 +142,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-
-
-        //val connectButton: Button = findViewById(R.id.connectButton)
-        //connectButton.setOnClickListener { connect() }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -101,14 +154,74 @@ class MainActivity : AppCompatActivity() {
                     // user denied to turn on bluetooth from popup
                     Toast.makeText(this, "Couldn't turn on bluetooth", Toast.LENGTH_LONG).show()
                 }
+
+            100 ->
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    // when success
+                    // init places
+                    var place : Place = Autocomplete.getPlaceFromIntent(data)
+                    // check condition
+                    if (sType.equals("source")){
+                        // when type is source
+                        // increase flag
+                        flag++
+                        //set address on edit text
+                        geo_source.setText(place.address)
+                        // get lat and long
+                        var sSource : String = place.latLng.toString()
+                        sSource = sSource.replace("lat/lng: ","")
+                        sSource = sSource.replace("(","")
+                        sSource = sSource.replace(")","")
+                        val split : List<String> = sSource.split(",")
+                        lat0 = split[0].toDouble()
+                        long0 = split[1].toDouble()
+                    }else{
+                        // when type is destination
+                        // increase flag value
+                        flag++
+                        // set address on edit text
+                        geo_destination.setText(place.address)
+                        // get lat and long
+                        var sDestination : String = place.latLng.toString()
+                        sDestination = sDestination.replace("lat/lng: ","")
+                        sDestination = sDestination.replace("(","")
+                        sDestination = sDestination.replace(")","")
+                        val split : List<String> = sDestination.split(",")
+                        lat1 = split[0].toDouble()
+                        long1 = split[1].toDouble()
+                    }
+
+                    // check condition
+                    if (flag >= 2) {
+                        distance(lat0,long0,lat1,long1);
+                    }
+                } else if (requestCode == AutocompleteActivity.RESULT_ERROR && data != null){
+                    // Error
+                    // init status
+                    var status : Status = Autocomplete.getStatusFromIntent(data)
+
+                    //Display Toast
+                    Toast.makeText(applicationContext,status.statusMessage, Toast.LENGTH_LONG).show()
+                }
         }
         super.onActivityResult(requestCode, resultCode, data)
     }
 
-    private fun connect() {
-        val pName = "String" // parse name from bluetooth
-        val pStatus : Int = 0 // parse status from bluetooth
-        val fence : Fence = Fence(pName, pStatus)
+    private fun distance (lat0 : Double, long0 : Double, lat1 : Double, long1 : Double) {
+        val longDiff : Double = long0 - long1
+        var dist : Double = Math.sin(deg2rad(lat0)) * Math.sin(deg2rad(lat1)) + Math.cos(deg2rad(lat0)) * Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(longDiff))
+        dist = Math.acos(dist)
+        dist = rad2deg(dist)
+        dist = dist * 1.609344
+        geo_distance.setText(dist.toString() + " Km")
+    }
+
+    private fun rad2deg (dist : Double) : Double {
+        return dist * 180.0 / Math.PI
+    }
+
+    private fun deg2rad (lat0 : Double) : Double {
+        return lat0*Math.PI/180.0
     }
 
 }
